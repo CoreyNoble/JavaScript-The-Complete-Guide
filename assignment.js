a1331d6d3a090312460e469b2c5d94beeaa9510b
Corey Noble <coreynoble@hotmail.ca>
2019-12-07 14:21:22 -0500

    Assignment 1 - Finished - Time to Practice: Variables, Constants, Operators & Core Data Types

---
+++ assignment.js
@@ -0,0 +1,11 @@
+let userInput = 15;
+let result;
+
+result = userInput + 18;
+
+result = result - 1;
+result = result * 2;
+result = result / 3;
+
+alert('Result: ' + result);
+alert('User Input: ' + userInput);

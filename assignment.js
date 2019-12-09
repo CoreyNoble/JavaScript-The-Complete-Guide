abba1844fe394fcda59e60a12d7c4cc94ac8f896
Corey Noble <coreynoble@hotmail.ca>
2019-12-08 20:13:40 -0500

    Assignment 2 - Finished - Time to Practice: Functions

--- assignment.js
+++ assignment.js
@@ -1,1 +1,20 @@
 const task3Element = document.getElementById('task-3');
+
+task3Element.addEventListener('click', outputTextOfChoice);
+
+function outputTextOfChoice(){
+    alert('Text of choice');
+}
+
+function outputName(name){
+    alert('Name: ' + name);
+}
+
+function takesInThreeParams(param1, param2, param3){
+    const result = param1 + ' ' + param2 + ' ' + param3;
+    alert(result);
+}
+
+outputTextOfChoice();
+outputName('Corey');
+takesInThreeParams('PARAM1', 'PARAM2', 'PARAM3');

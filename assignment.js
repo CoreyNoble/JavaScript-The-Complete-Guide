d4ed2262ff679e4221f5e3771d96eed77f443d8a
Corey Noble <coreynoble@hotmail.ca>
2019-12-24 23:15:55 -0500

    Assignment 4 - Adding Instructor Examples - Time to Practice: Functions

--- assignment.js
+++ assignment.js
@@ -1,9 +1,9 @@
 // 1
-const sayHello1 = (name = 'User') => console.log('Hi ' + name);
+const sayHello1 = name => console.log('Hi ' + name);
 sayHello1('Corey');
 
 // 2
-const sayHello2a = (intro, name) => console.log(intro + ' ' + name);
+const sayHello2a = (name, intro = 'Hi') => console.log(intro + ' ' + name);
 sayHello2a('Hello', 'Corey');
 
 const sayHello2b = () => console.log('Welcome 1');
@@ -13,21 +13,37 @@ const sayHello2c = () => 'Welcome 2';
 console.log(sayHello2c());
 
 // 3
-sayHello1();
+sayHello2a('Corey');
 
 // 4
 const checkInput = (cb, ...args) => {
+  let hasEmptyText = false;
+
   for (const arg of args) {
-    if (arg === '') {
+    if (!arg) {
       console.log(`Error: an argument that was passed in contains an empty string`);
-      return;
+      hasEmptyText = true;
+      break;
     }
   }
 
-  cb();
+  if (!hasEmptyText) {
+    cb();
+  }
 };
 
 const allStrings = () => console.log('Success: Calback fired, No arguments contain any empty strings');
+
+checkInput(
+  () => {
+    console.log('All not empty!');
+  },
+  'Hello',
+  '12',
+  'adsfa',
+  'Not empty',
+  ''
+);
 
 checkInput(allStrings, 'a', 'b', 'c', 'd', 'e');
 checkInput(allStrings, '', '', 'c', 'd', 'e');

a4327b7be56eb5607b5ebdfd9c9e47131aea356b
Corey Noble <coreynoble@hotmail.ca>
2019-12-26 18:23:36 -0500

    Assignment 5 - Finished - Time to Practice: DOM Querying

---
+++ assignment.js
@@ -0,0 +1,17 @@
+// 1
+let item1a = document.getElementById('task-1');
+item1a.style.backgroundColor = 'black';
+
+let item1b = document.querySelector('#task-1');
+item1b.style.color = 'white';
+
+// 2
+let title2a = document.querySelector('title');
+title2a.textContent = 'Assignment 2a - Solved!';
+
+let title2b = document.querySelector('head').title;
+title2b.title = 'Assignment 2b - Solved';
+
+// 3
+const h1 = document.querySelector('h1');
+h1.textContent = 'Assignment 3 - Solved!';

1cd01e721d5637abe3ee4f07dd9aa4c0ec6d1568
Corey Noble <coreynoble@hotmail.ca>
2019-12-27 22:29:06 -0500

    198 - Where Arrow Functions Shine!

--- app.js
+++ app.js
@@ -111,8 +111,6 @@ const sortedPrices = prices.sort((a, b) => {
 // console.log(sortedPrices.reverse());
 console.log(sortedPrices);
 
-const filteredArray = prices.filter((price, index, prices) => {
-  return price > 6;
-});
+const filteredArray = prices.filter(p => p > 6);
 
 console.log(filteredArray);
